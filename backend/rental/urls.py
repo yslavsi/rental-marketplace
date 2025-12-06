9a1d65c590fa5a5db68c1c32e61342765087ac7c
yslavsi <sibilenkoyaroslav3@gmail.com>
2025-12-07 01:21:49 +0300

    FINAL: Add migrations and basic URLs/views

--- backend/rental/urls.py
+++ backend/rental/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.shortcuts import redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from rental.views import home
+from rental.views import health_check
 from . import views
 
 def user_logout(request):
